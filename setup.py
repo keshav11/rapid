a9eff2085c85b4746167658f3efdeb0c2a45e9fc
Keshav Gupta <keshav1032@gmail.com>
2015-12-04 14:11:25 +0530

    fixed some bugs related to newly added languages

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
     name = "rapid",
-    version = "0.0.1",
+    version = "0.0.2",
     author = "Keshav Gupta",
     author_email = "keshav@keshav.xyz",
     description = ("rapid generates boilerplate code through command line"),
@@ -14,14 +14,17 @@ setup(
     keywords = ["boilerplate", "cli", "generate" "command line"],
     url = "https://github.com/keshav11/rapid",
     packages=['rapid'],
-    long_description=read('README.md'),
-    download_url='https://github.com/keshav11/rapid/tarball/0.0.1',
+    download_url='https://github.com/keshav11/rapid/tarball/0.0.2',
     package_data={
         'rapid': [
         'boilerplate/c/*.bp',
         'boilerplate/csharp/*.bp',
         'boilerplate/html/*.bp',
+        'boilerplate/cpp/*.bp',
+        'boilerplate/empty/*.bp',
+        'boilerplate/python/*.bp',
         'boilerplate/java/*.bp'
+
         ]
     },
       entry_points = {
